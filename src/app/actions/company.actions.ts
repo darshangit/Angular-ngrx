644cbe07a071694d1a82f4e1a61ff81fb4babc69
darshangit <darshans1989@gmail.com>
2018-03-25 17:34:47 +0530

    Redux pattern

---
+++ src/app/actions/company.actions.ts
@@ -0,0 +1,17 @@
+import { Company } from '../modals/company';
+
+export const LOAD_COMPANIES = 'LOAD_COMPANIES;';
+export const LOAD_COMPANIES_SUCCESS = 'LOAD_COMPANIES_SUCCESS;';
+
+export class LoadCompaniesAction {
+  readonly type = LOAD_COMPANIES;
+  constructor() {}
+}
+
+export class LoadCompaniesSuccessAction {
+  readonly type = LOAD_COMPANIES_SUCCESS;
+
+  constructor(public payload: Company[]) {}
+}
+
+export type Action = LoadCompaniesAction | LoadCompaniesSuccessAction;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
-
+import { companyReducer } from './reducers/company.reducer';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, StoreModule.forRoot({companies: companyReducer})],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
